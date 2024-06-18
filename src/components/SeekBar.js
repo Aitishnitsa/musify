f166255c02ac0ee78decff19f4abbe866ac5ac05
Aitishnitsa <aitishnitsa@gmail.com>
2024-06-18 10:56:19 +0300

    handle seek

--- src/components/SeekBar.js
+++ src/components/SeekBar.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { msToTime } from "../config";
+import { msToTime, fetchSeek } from "../config";
 
 const SeekBar = ({ player, progress, duration }) => {
     const [lineWidth, setLineWidth] = useState(0);
@@ -8,12 +8,22 @@ const SeekBar = ({ player, progress, duration }) => {
         setLineWidth((progress * 100) / duration);
     }, [progress, duration]);
 
+    const handleSeek = async (e) => {
+        e.preventDefault();
+        const seekBar = e.currentTarget;
+        const clickPosition = e.clientX - seekBar.getBoundingClientRect().left;
+        const newProgress = Math.round((clickPosition / seekBar.offsetWidth) * duration);
+        await fetchSeek(newProgress);
+    };
+
     const currentTime = player ? msToTime(progress) : "00:00";
     const trackLength = player ? msToTime(duration) : "00:00";
 
     return (
         <div className='mt-2'>
-            <div className="w-full bg-gray-700 rounded-full h-1.5 cursor-pointer">
+            <div className="w-full bg-gray-700 rounded-full h-1.5 cursor-pointer"
+                onClick={handleSeek}
+            >
                 <div style={{ width: `${lineWidth}%` }} className="bg-white h-1.5 rounded-full"></div>
             </div>
             <div className="flex justify-between text-sm text-white">
@@ -22,6 +32,6 @@ const SeekBar = ({ player, progress, duration }) => {
             </div>
         </div>
     );
-}
+};
 
 export default SeekBar;
--- src/config.js
+++ src/config.js
@@ -48,3 +48,4 @@ export const fetchPlayPause = (action) => fetchWithToken(`https://api.spotify.co
 export const fetchNextPrevious = (action) => fetchWithToken(`https://api.spotify.com/v1/me/player/${action}`, "POST");
 export const fetchPlaylistsTracks = (playlist_id) => fetchWithToken(`https://api.spotify.com/v1/playlists/${playlist_id}`, "GET");
 export const fetchAddToQueue = (uri) => fetchWithToken(`https://api.spotify.com/v1/me/player/queue?uri=${uri}`, "POST");
+export const fetchSeek = (progress) => fetchWithToken(`https://api.spotify.com/v1/me/player/seek?position_ms=${progress}`, "PUT");
